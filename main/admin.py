c388080e4532af68e3cf186e047ef6b49b6ae78b
suhanidesale <deslesuhani02@gmail.com>
2022-09-02 10:48:54 +0530

    models created

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category , Branch, Competition, CompetitionAttribute, Domain , Year, slider
 
 admin.site.register(Category)
+admin.site.register(Branch)
+admin.site.register(Year)
+admin.site.register(slider)
+admin.site.register(CompetitionAttribute)
+
+class CompetitionAdmin(admin.ModelAdmin):
+    list_display = ('id','title','branch','year' ,'domain','reg_fee','status')
+    list_editable = ('status' , )
+admin.site.register(Competition , CompetitionAdmin)
 
 # Register your models here.
--- main/models.py
+++ main/models.py
@@ -1,5 +1,3 @@
-from distutils.command import upload
-from email.mime import image
 from django.db import models
 
 # Create your models here.
@@ -11,3 +9,51 @@ class Category(models.Model):
     def __str__(self):
         return self.title
 
+class Branch(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to = "branch_imgs/")
+
+    def __str__(self):
+        return self.title
+
+class Year(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to = "year_imgs/")
+
+    def __str__(self):
+        return self.title
+
+
+class Domain(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to = "domain_imgs/")
+
+    def __str__(self):
+        return self.title
+
+class Competition(models.Model):
+    title = models.CharField(max_length=200)
+    image=models.ImageField(upload_to = "competition_imgs/")
+    slug=models.CharField(max_length=400)
+    detail=models.TextField()
+    specifications=models.TextField()
+    reg_fee=models.PositiveIntegerField()
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    branch = models.ForeignKey(Branch,on_delete=models.CASCADE)
+    year = models.ForeignKey(Year,on_delete=models.CASCADE)
+    domain = models.ForeignKey(Domain,on_delete=models.CASCADE)
+    status = models.BooleanField(default=True)
+
+class slider(models.Model):
+    img = models.CharField(max_length=200)
+    alt_text = models.CharField(max_length=300)
+
+# Competition Attributes
+class CompetitionAttribute(models.Model):
+    competition = models.ForeignKey(Competition,on_delete=models.CASCADE)
+    domain = models.ForeignKey(Domain,on_delete=models.CASCADE)
+    year = models.ForeignKey(Year,on_delete=models.CASCADE)
+    reg_fee = models.ForeignKey(Competition, on_delete=models.CASCADE , related_name='reg_fees')
+
+    def __str__(self):
+        return self.competition.title 
