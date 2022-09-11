84fec180a57cae74091fe78075e7319abf912445
suhanidesale <deslesuhani02@gmail.com>
2022-09-12 01:35:46 +0530

    all competitions layout loaded

--- main/urls.py
+++ main/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('',views.home,name='home'),
     path('category-list',views.category_list,name='category-list'),
     path('branch-list',views.branch_list,name='branch-list'),
+    path('competition-list',views.competition_list,name='competition-list'),
 ]
   
 if settings.DEBUG:
--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Branch, Category
+from .models import Branch, Category , Competition
 # Create your views here.
 
 #home page
@@ -12,11 +12,13 @@ def category_list(request):
     data_cat = Category.objects.all().order_by('-id')
     return render(request , 'category_list.html' , {'data_cat':data_cat})
 
+#brand
 def branch_list(request):
     data_branch = Branch.objects.all().order_by('-id')
     return render(request , 'branch_list.html' , {'data_branch':data_branch})
 
+def competition_list(request):
+    data_competition = Competition.objects.all().order_by('-id')
+    return render(request , 'competition_list.html' , {'data_competition':data_competition})
 
-
-#branch
 
