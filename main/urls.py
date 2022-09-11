dbfdb25fc9cf7e0270da93e94d085d663c4173df
suhanidesale <deslesuhani02@gmail.com>
2022-09-11 22:33:31 +0530

    various content loaded in categories and branches

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,14 @@
-from django.urls import path
+from django.urls import path , include
 from . import views
+
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('',views.home,name='home'),
+    path('category-list',views.category_list,name='category-list'),
+    path('branch-list',views.branch_list,name='branch-list'),
 ]
+  
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- main/views.py
+++ main/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
-
+from .models import Branch, Category
 # Create your views here.
 
 #home page
 
 def home(request):
     return render(request , 'index.html')
+
+#category
+def category_list(request):
+    data_cat = Category.objects.all().order_by('-id')
+    return render(request , 'category_list.html' , {'data_cat':data_cat})
+
+def branch_list(request):
+    data_branch = Branch.objects.all().order_by('-id')
+    return render(request , 'branch_list.html' , {'data_branch':data_branch})
+
+
+
+#branch
+
